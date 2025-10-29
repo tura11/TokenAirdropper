16a37f55e0f6aa45fd26026bb248c98fbd11c79b
Tymek <tymek_uramek@wp.pl>
2025-10-29 20:11:04 +0100

    Add wallet logic

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,23 @@
 "use client"
 
 import {type ReactNode} from "react"
-import config from "@/rainbowKitConfig"
+import config from "../rainbowKitConfig"
 import {WagmiProvider} from "wagmi"
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit"
-
+import { RainbowKitProvider, ConnectButton } from "@rainbow-me/rainbowkit"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { useState } from "react"
+import "@rainbow-me/rainbowkit/styles.css"
 
 export function Providers(props: {children: ReactNode}) {
+    const [queryClient] = useState(() => new QueryClient())
     return(
         <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
             <RainbowKitProvider>
+                <ConnectButton />
                 {props.children}
             </RainbowKitProvider>
+            </QueryClientProvider>
            
         </WagmiProvider>
     )
