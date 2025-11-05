c71214575920fa0ab76ab4f1eacdbe59568faf0d
Tymek <tymek_uramek@wp.pl>
2025-11-05 12:35:19 +0100

    Deploy MockToken and MockTSender

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,23 +3,33 @@ pragma solidity ^0.8.24;
 
 import "forge-std/Script.sol";
 import {MockToken} from "../src/MockToken.sol";
-import {MockTSender} from  "../src/MockTsender.sol";
+import {MockTSender} from "../src/MockTSender.sol";
 
 contract Deploy is Script {
     function run() external {
-        // start broadcast (deploy z konta[0])
-        vm.startBroadcast();
 
-        // Deploy mock token
+        uint256 deployerPrivateKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
+
+        vm.startBroadcast(deployerPrivateKey);
+
+   
         MockToken mock = new MockToken(1000 * 10 ** 18);
         console.log("MockToken deployed at:", address(mock));
 
-        // Deploy TSender
+
         MockTSender tsender = new MockTSender();
-        console.log("TSender deployed at:", address(tsender));
+        console.log("MockTSender deployed at:", address(tsender));
 
         vm.stopBroadcast();
+
+        console.log("------------------------------");
+        console.log("MockToken:", address(mock));
+        console.log("MockTSender:", address(tsender));
+        console.log("------------------------------");
     }
 }
-// MockToken deployed at: 0x5FbDB2315678afecb367f032d93F642f64180aa3
- // TSender deployed at: 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+
+
+//   MockToken deployed at: 0x851356ae760d987E095750cCeb3bC6014560891C
+//   MockTSender deployed at: 0xf5059a5D33d5853360D16C683c16e67980206f36
